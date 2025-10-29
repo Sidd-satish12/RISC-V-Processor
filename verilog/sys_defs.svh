151930d3ff373aa2e65f0a7e0d0466a188e844c7
Lance Ying <lanying@umich.edu>
2025-10-29 19:08:06 -0400

    fix sys_defs.svh

--- verilog/sys_defs.svh
+++ verilog/sys_defs.svh
@@ -311,23 +311,19 @@ typedef enum logic [3:0] {
     OPB_IS_J_IMM  = 4'h5
 } ALU_OPB_SELECT;
 
-////////////////////////////////////////
-// ----      Major Structures    ---- //
-////////////////////////////////////////
-
-// ALU function code
-typedef enum logic [3:0] {
-    ADD,
-    SUB,
-    AND,
-    SLT,
-    SLTU,
-    OR,
-    XOR,
-    SLL,
-    SRL,
-    SRA
-} ALU_FUNC;
+// // ALU function code
+// typedef enum logic [3:0] {
+//     ALU_ADD     = 4'h0,
+//     ALU_SUB     = 4'h1,
+//     ALU_SLT     = 4'h2,
+//     ALU_SLTU    = 4'h3,
+//     ALU_AND     = 4'h4,
+//     ALU_OR      = 4'h5,
+//     ALU_XOR     = 4'h6,
+//     ALU_SLL     = 4'h7,
+//     ALU_SRL     = 4'h8,
+//     ALU_SRA     = 4'h9
+// } ALU_FUNC;
 
 // MULT funct3 code
 // we don't include division or rem options
