640fb007eaabd74413547d81d66e941814250362
Julian Carrier <juliankc@umich.edu>
2025-11-01 13:28:02 -0400

    rewrite rob testbench, add mispredict signals

--- verilog/rob.sv
+++ verilog/rob.sv
@@ -16,18 +16,18 @@
 `include "sys_defs.svh"
 
 module rob (
-    input logic                     clock,
-    input logic                     reset, // reset on mispredict
+    input logic clock,
+    input logic reset,  // reset on mispredict
 
     // Dispatch
-    input  ROB_ENTRY  [`N-1:0]      rob_entry_packet,
-    output logic [$clog2(`ROB_SZ+1)-1:0] free_slots,
+    input  ROB_ENTRY [               `N-1:0] rob_entry_packet,
+    output logic     [$clog2(`ROB_SZ+1)-1:0] free_slots,
 
     // Complete
-    input  ROB_UPDATE_PACKET        rob_update_packet,
+    input ROB_UPDATE_PACKET rob_update_packet,
 
     // Retire
-    output ROB_ENTRY [`N-1:0]       head_entries, // Could be retired
+    output ROB_ENTRY [`N-1:0] head_entries  // Could be retired
 );
     ROB_ENTRY [`ROB_SZ-1:0] rob_entries, rob_entries_next;
     logic [$clog2(`ROB_SZ+1)-1:0] free_count, free_count_next;
@@ -36,7 +36,7 @@ module rob (
 
     // For calculating free count
     logic retire;
-    logic [`N-1:0]  next_N_complete_bits;
+    logic [`N-1:0] next_N_complete_bits;
     logic [$clog2(`N+1)-1:0] num_retired, num_dispatched;
 
     assign retire = &next_N_complete_bits;
@@ -49,25 +49,25 @@ module rob (
         for (int i = 0; i < `N; i++) begin
             // Dispatch, assume incoming valid instructions to be contiguous from index 0
             if (rob_entry_packet[i].valid) begin
-                rob_entries_next[(tail_idx + i) % `ROB_SZ] = rob_entry_packet[i];
+                rob_entries_next[(tail_idx+i)%`ROB_SZ] = rob_entry_packet[i];
             end
 
             // Complete ROB entries update
             if (rob_update_packet.valid[i]) begin
-                rob_entries_next[rob_update_packet.idx[i]].value = rob_update_packet.values[i];
                 rob_entries_next[rob_update_packet.idx[i]].complete = 1'b1;
+                rob_entries_next[rob_update_packet.idx[i]].mispredict = rob_update_packet.mispredicts[i];
                 rob_entries_next[rob_update_packet.idx[i]].branch_taken = rob_update_packet.branch_taken[i];
                 rob_entries_next[rob_update_packet.idx[i]].branch_target = rob_update_packet.branch_targets[i];
             end
 
             // For determining whether to retire
-            head_entries[i] = rob_entries[(head_idx + i) % `ROB_SZ];
-            if (head_entries[i].complete) begin 
+            head_entries[i] = rob_entries[(head_idx+i)%`ROB_SZ];
+            if (head_entries[i].complete) begin
                 next_N_complete_bits[i] = 1'b1;
             end
 
             if (retire) begin
-                rob_entries_next[(head_idx + i) % `ROB_SZ].valid = 1'b0;
+                rob_entries_next[(head_idx+i)%`ROB_SZ].valid = 1'b0;
             end
 
             // Free Count calculation
