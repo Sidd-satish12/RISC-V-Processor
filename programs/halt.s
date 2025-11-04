aa50c32ef7f6d584844d017666a3314f62cf0b7e
Siddarth Satish <sidd.satish04@gmail.com>
2025-11-04 15:07:03 -0500

    changed allocator to OR instead of XOR for resource status, need to further test this on mult instructions, but currently no uneccessary stalls on addi instructions resulting in good CPI

--- programs/halt.s
+++ programs/halt.s
@@ -6,6 +6,11 @@
     addi x11, x12, 6
     addi x13, x14, 7
     addi x15, x16, 8
+    addi x17, x18, 9
+    addi x19, x20, 10
+    addi x21, x22, 11
+    addi x23, x24, 12
+    addi x25, x26, 13
     wfi
     
 
