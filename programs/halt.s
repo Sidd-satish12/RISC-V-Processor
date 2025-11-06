64893c6e14d8674ba22e102bdfa3860a67719004
Siddarth Satish <sidd.satish04@gmail.com>
2025-11-05 19:34:20 -0500

    fixed branch module to function correctly

--- programs/halt.s
+++ programs/halt.s
@@ -1,6 +1,11 @@
-    addi x1, x2, 1
-    addi x3, x4, 2
-    addi x5, x3, 3
-    wfi
+    addi x1, x0, 5      # x1 = 5
+    addi x2, x0, 0      # x2 = 0
+    beq  x1, x0, skip   # if x1 == 0, skip next instruction (should NOT branch)
+    addi x2, x2, 1      # x2 = x2 + 1 (should execute)
+skip:
+    addi x1, x1, -1     # x1 = x1 - 1
+    bne x1, x0, skip       # if x1 != 0, branch to loop (here we treat next instruction as loop)
+    wfi                  # end
+
     
 
