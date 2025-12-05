8f02b8b801f5080eec86d417e8f7e1c2f19f164a
Julian Carrier <juliankc@umich.edu>
2025-12-05 01:56:30 -0500

    increase cycles again

--- test/cpu_test.sv
+++ test/cpu_test.sv
@@ -36,7 +36,7 @@ import "DPI-C" function string decode_inst(int inst);
 // import "DPI-C" function void close_pipeline_output_file();
 
 
-`define TB_MAX_CYCLES 10000000
+`define TB_MAX_CYCLES 1000000000
 
 
 module testbench;
